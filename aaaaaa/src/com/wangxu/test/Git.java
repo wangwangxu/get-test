4610e18380f28ef7b14dfbfde375beabbf217eee
wangx <wangx@qq.com>
2019-03-22 14:26:06 +0800

    修改gitclass

--- aaaaaa/src/com/wangxu/test/Git.java
+++ aaaaaa/src/com/wangxu/test/Git.java
@@ -8,10 +8,13 @@ public class Git {
         System.out.println("hell");
         System.out.println("hell");
         System.out.println("hell");
+
+        //hello test
     }
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 
 }
